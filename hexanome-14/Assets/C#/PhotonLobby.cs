9d6655647ce5b57a227f7f8bc23b19a9883e4740
anahita-m <mohapatra.anahita@gmail.com>
2020-03-05 14:52:26 -0500

    adding multiplayer scripts - not working yet

---
+++ hexanome-14/Assets/C#/PhotonLobby.cs
@@ -0,0 +1,95 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonLobby : MonoBehaviourPunCallbacks, ILobbyCallbacks
+{
+    public static PhotonLobby lobby;
+
+    //public GameObject joinButton;
+    //public GameObject cancelButton;
+    public string roomName;
+    public int roomSize;
+    public GameObject roomListingPrefab;
+    public Transform roomsPanel;
+
+    private void Awake()
+    {
+        lobby = this; //Creates the singleton, lives withtin the Main menu scene
+    }
+    // Use this for initialization
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Player has connected to the master photon server");
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        base.OnRoomListUpdate(roomList);
+        RemoveRoomListings();
+        foreach(RoomInfo room in roomList)
+        {
+            ListRoom(room);
+        }
+    }
+
+    void RemoveRoomListings()
+    {
+        while(roomsPanel.childCount != 0)
+        {
+            Destroy(roomsPanel.GetChild(0).gameObject);
+        }
+    }
+
+    void ListRoom(RoomInfo room)
+    {
+        if(room.IsOpen && room.IsVisible)
+        {
+            GameObject tempListing = Instantiate(roomListingPrefab, roomsPanel);
+            RoomButton tempButton = tempListing.GetComponent<RoomButton>();
+            tempButton.roomName = room.Name;
+            tempButton.roomSize = room.MaxPlayers;
+            tempButton.SetRoom();
+        }
+    }
+
+    void CreateRoom()
+    {
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)roomSize };
+        PhotonNetwork.CreateRoom("Room" + roomName, roomOps);
+
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to create a room but failed. There must already be a room with the same name");
+        //CreateRoom();
+        //Write something to inform user to change room name
+    }
+
+    public void OnRoomNameChanged(string nameIn)
+    {
+        roomName = nameIn;
+    }
+
+    public void OnRoomSizeChanged(string sizeIn)
+    {
+        roomSize = int.Parse(sizeIn);
+    }
+
+    public void JoinLobbyOnClick()
+    {
+        if (!PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.JoinLobby();
+        }
+    }
+
+}
